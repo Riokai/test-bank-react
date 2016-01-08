9398423ccc732a808b902eb6017a9802eb995d33
Kai <zengkai2009@foxmail.com>
2016-01-08 10:27:14 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import 'core-js/fn/object/assign';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/Main';
+
+import Test from './components/TestComponent'
+
+import { Router, Route, Link, browserHistory } from 'react-router'
+
+// Render the main component into the dom
+// ReactDOM.render(<App />, document.getElementById('app'));
+
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}></Route>
+    <Route path="/test" component={Test}></Route>
+  </Router>
+), document.body)
