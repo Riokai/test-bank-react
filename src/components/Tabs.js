2ef8fae9eb01a284e9c98306efe6ddc21afc75f3
Kai <zengkai2009@foxmail.com>
2016-03-01 10:35:31 +0800

    add Tabs component

---
+++ src/components/Tabs.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+class Tabs extends React.Component {
+  render() {
+    return (
+      <div className="row">
+        <div className="col-xs-12">
+
+        </div>
+        <Breadcrumb />
+      </div>
+    )
+  }
+}
+
+export default Tabs
