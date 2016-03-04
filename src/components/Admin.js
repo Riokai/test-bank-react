c51e892a9a07b5ed52b90de480f64401a7ae4ace
Kai <zengkai2009@foxmail.com>
2016-03-04 11:05:37 +0800

    add Admin page component

---
+++ src/components/Admin.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import { NavTab, NavTabItem } from './NavTab'
+import Selector from './Selector'
+import MultipAdd from './MultipAdd'
+
+export class ScheduleBuild extends React.Component {
+
+  render() {
+    console.log('build');
+
+    const data = [
+      {title: '单个添加'},
+      {title: '批量添加'}
+    ]
+
+    return (
+      <div>
+        <NavTab items={data}>
+          <NavTabItem>
+            <Selector title="选择教师" />
+            <Selector title="选择课程" />
+            <Selector title="选择时间" />
+          </NavTabItem>
+          <NavTabItem>
+            <MultipAdd />
+          </NavTabItem>
+        </NavTab>
+      </div>
+    )
+  }
+}
