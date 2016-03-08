61db663dbf4e1da45ce839c11b16ac42aeb1b5df
Kai <zengkai2009@foxmail.com>
2016-03-08 15:53:36 +0800

    add dev tools

---
+++ src/containers/DevTools.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { createDevTools } from 'redux-devtools'
+import LogMonitor from 'redux-devtools-log-monitor'
+import DockMonitor from 'redux-devtools-dock-monitor'
+import MultipleMonitors from 'redux-devtools-multiple-monitors'
+import Dispatcher from 'redux-devtools-dispatch'
+
+import * as actionCreators from '../actions/user'
+
+export default createDevTools(
+  <DockMonitor toggleVisibilityKey='ctrl-h'
+               changePositionKey='ctrl-q'>
+    <MultipleMonitors>
+      <LogMonitor />
+      <Dispatcher actionCreators={actionCreators} initEmpty />
+    </MultipleMonitors>
+  </DockMonitor>
+);
